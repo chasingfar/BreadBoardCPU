695e7bff5fa90c498b2cf78da18d11d75b600fb1
chasingfar <chasingfar@gmail.com>
2021-02-02 10:11:31 +0000

    replace If() by struct IF{}

--- include/asm/advance.h
+++ include/asm/advance.h
@@ -21,26 +21,32 @@ namespace BreadBoardCPU::ASM {
 			return asm_ >> block.label << block.body;
 		}
 	};
-	code_t If(code_t cond,code_t if_true,code_t if_false={}){
-		Label label_false,label_end;
-		return {
-			cond,
-			brz(label_false),
-			if_true,
-			jmp(label_end),
-			label_false,
-			if_false,
-			label_end,
-		};
-	}
+	struct IF{
+		code_t cond;
+		code_t if_true;
+		code_t if_false={};
+		friend ASM &operator<<(ASM &asm_, IF if_) {
+			Label label_false{"if_false"},label_end{"if_end"};
+			auto [cond,if_true,if_false]=if_;
+			return asm_
+				<<cond
+				<<brz(label_false)
+				<<if_true
+				<<jmp(label_end)
+				>>label_false
+				<<if_false
+				>>label_end
+			;
+		}
+	};
 
 	auto test_if(){
 		ASM program{};
 		return program
-			<<If({imm(0)},
+			<<IF{{imm(0)},
 			     {imm(Reg::A,1)},
 			     {imm(Reg::A,2)}
-			)
+			}
 			<<imm(Reg::B,3)
 			<<halt()
 			<<ASM::END
