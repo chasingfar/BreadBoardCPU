28e029a0775073607b21aa032d859a0c6f937c3c
chasingfar <chasingfar@gmail.com>
2021-07-15 01:30:16 +0800

    add test load and save

--- test/basic.cpp
+++ test/basic.cpp
@@ -13,6 +13,25 @@ void run_op(CPU& cpu,const code_t& code){
 	cpu.tick_op();
 }
 
+TEST_CASE("load and save","[asm][basic]"){
+	CPU cpu;
+	cpu.tick_op();
+
+	cpu.load(ops_t{12,34},0xABCD);
+	cpu.REG[CPU::Reg::B.v()]=0xAB;
+	cpu.REG[CPU::Reg::A.v()]=0xCD;
+
+	run_op(cpu,load(Reg16::BA));
+	REQUIRE(*cpu.get_pointer(CPU::Reg16::SP,1) == 12);
+	run_op(cpu,load(Reg16::BA,1));
+	REQUIRE(*cpu.get_pointer(CPU::Reg16::SP,1) == 34);
+
+	run_op(cpu,save(Reg16::BA));
+	REQUIRE(cpu.RAM[0xABCD]==34);
+	run_op(cpu,save(Reg16::BA,1));
+	REQUIRE(cpu.RAM[0xABCE]==12);
+}
+
 TEST_CASE("push and pop","[asm][basic]"){
     CPU cpu;
     cpu.tick_op();
