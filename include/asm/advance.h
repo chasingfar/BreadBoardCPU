70b026f7f5608b4e04711414d9e64f682e99bae5
chasingfar <chasingfar@gmail.com>
2021-07-23 06:54:55 +0000

    make Block-like implicit convert to coed_t

--- include/asm/advance.h
+++ include/asm/advance.h
@@ -22,51 +22,35 @@ namespace BreadBoardCPU::ASM {
 			body << std::move(code);
 			return *this;
 		}
-
-		friend ASM &operator<<(ASM &asm_, const Block& block) {
-			return asm_ >> block.start << block.body >> block.end;
+		operator code_t(){
+			return {start,body,end};
 		}
 	};
 	struct IF{
 		code_t cond;
 		Block if_true;
 		Block if_false{};
-		friend ASM &operator<<(ASM &asm_, IF if_) {
-			auto [cond,if_true,if_false]=std::move(if_);
-			return asm_
-				<<cond
-				<<brz(if_false.start)
-				<<if_true
-				<<jmp(if_false.end)
-				<<if_false
-			;
+		operator code_t(){
+			return {
+				cond,
+				brz(if_false.start),
+				if_true,
+				jmp(if_false.end),
+				if_false,
+			};
 		}
 	};
 	struct While{
 		Block cond;
 		Block body{};
-		friend ASM &operator<<(ASM &asm_, While while_) {
-			auto [cond,body]=std::move(while_);
-			return asm_
-					<<cond
-					<<brz(body.end)
-					<<body
-					<<jmp(cond.start)
-					;
+		operator code_t(){
+			return {
+				cond,
+				brz(body.end),
+				body,
+				jmp(cond.start),
+			};
 		}
 	};
-
-	inline auto test_if(){
-		ASM program{};
-		return program
-			<<IF{{imm(0)},
-			     {imm(Reg::A,1)},
-			     {imm(Reg::A,2)}
-			}
-			<<imm(Reg::B,3)
-			<<halt()
-			<<ASM::END
-			;
-	}
 }
 #endif //BREADBOARDCPU_ADVANCE_H
