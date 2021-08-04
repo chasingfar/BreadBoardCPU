6d8e4586f370255482155c2c738b518857a16fe4
chasingfar <chasingfar@gmail.com>
2021-08-04 10:18:02 +0000

    expand operator macros

--- include/asm/operator.h
+++ include/asm/operator.h
@@ -7,56 +7,60 @@
 #include "basic.h"
 namespace BreadBoardCPU::ASM {
 
-#define ZERO_SHRINK                         \
-	for (addr_t i = 1; i < tmp.size; ++i) { \
-		tmp.push<<OR();                     \
-	}
-#define ZERO_REVERSE                        \
-	Label if_zero,end;                      \
-	tmp.push<<code_t{                       \
-		brz(if_zero),                       \
-		imm(0),                             \
-		jmp(end),                           \
-		if_zero,                            \
-		imm(1),                             \
-		end,                                \
-	}
-#define SUB_CMP(ge_zero,lt_zero)            \
-	Label if_ge_zero,end;                   \
-	tmp.push<<code_t{                       \
-		adj(tmp.size),                      \
-		brc(if_ge_zero),                    \
-		imm(lt_zero),                       \
-		jmp(end),                           \
-		if_ge_zero,                         \
-		imm(ge_zero),                       \
-		end,                                \
-	}
 	inline RValue operator!(const RValue& lhs){
 		RValue tmp{lhs};
-		ZERO_SHRINK;
-		ZERO_REVERSE;
-		return tmp;
+		for (addr_t i = 1; i < tmp.size; ++i) {
+			tmp.push << OR();
+		}
+		Label if_zero, end;
+		tmp.push << code_t{
+			brz(if_zero),
+			imm(0),
+			jmp(end),
+			if_zero,
+			imm(1),
+			end,
+		};
+		return {1,tmp.push,false};
 	}
 	inline RValue operator!=(const RValue& lhs,const RValue& rhs){
 		RValue tmp{sub(lhs,rhs)};
-		ZERO_SHRINK;
+		for (addr_t i = 1; i < tmp.size; ++i) {
+			tmp.push << OR();
+		}
 		return tmp;
 	}
 	inline RValue operator==(const RValue& lhs,const RValue& rhs){
-		RValue tmp{lhs!=rhs};
-		ZERO_REVERSE;
-		return tmp;
+		return !(lhs!=rhs);
 	}
+
 	inline RValue operator>=(const RValue& lhs,const RValue& rhs){
 		RValue tmp{sub(lhs,rhs)};
-		SUB_CMP(1,0);
-		return tmp;
+		Label if_ge_zero, end;
+		tmp.push << code_t{
+			adj(tmp.size),
+			brc(if_ge_zero),
+			imm(0),
+			jmp(end),
+			if_ge_zero,
+			imm(1),
+			end,
+		};
+		return {1,tmp.push,false};
 	}
 	inline RValue operator<(const RValue& lhs,const RValue& rhs){
 		RValue tmp{sub(lhs,rhs)};
-		SUB_CMP(0,1);
-		return tmp;
+		Label if_ge_zero, end;
+		tmp.push << code_t{
+			adj(tmp.size),
+			brc(if_ge_zero),
+			imm(1),
+			jmp(end),
+			if_ge_zero,
+			imm(0),
+			end,
+		};
+		return {1,tmp.push,false};
 	}
 	inline RValue operator<=(const RValue& lhs,const RValue& rhs){
 		return rhs>=lhs;
@@ -64,6 +68,7 @@ namespace BreadBoardCPU::ASM {
 	inline RValue operator>(const RValue& lhs,const RValue& rhs){
 		return rhs<lhs;
 	}
+
 	inline RValue operator+(const RValue& lhs,const RValue& rhs){
 		return add(lhs,rhs);
 	}
@@ -96,8 +101,5 @@ namespace BreadBoardCPU::ASM {
 	inline RValue operator~(const RValue& lhs){
 		return NOT(lhs);
 	}
-#undef ZERO_SHRINK
-#undef ZERO_REVERSE
-#undef SUB_CMP
 }
 #endif //BREADBOARDCPU_OPERATOR_H
