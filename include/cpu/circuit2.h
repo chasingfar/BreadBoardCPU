7f8ec9f677ad674f31ae1b22a8d1b7146807b9c3
chasingfar <chasingfar@gmail.com>
2022-06-20 17:15:26 +0000

    remove pin, work on Port

--- include/cpu/circuit2.h
+++ include/cpu/circuit2.h
@@ -4,6 +4,8 @@
 
 #ifndef BBCPU_CIRCUIT_H
 #define BBCPU_CIRCUIT_H
+#include <exception>
+#include <optional>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -15,11 +17,6 @@ namespace Circuit{
 		INPUT,
 		OUTPUT,
 	};
-	struct Component{
-		void update(){}
-	};
-	struct Pin;
-
 	struct Wire{
 		enum Level:int{
 			Low      = -2,
@@ -53,37 +50,40 @@ Z	L	H	D	U	Z
 			return val>0?1:0;
 		}
 	};
-	struct Pin{
-		Wire* wire;
-		void set(bool v){
-
-		}
-		void pull(bool v){
-
-		}
-		bool get(bool v){
-
-		}
-		Pin& operator =(bool v){
-			return *this;
-		}
-	};
+	struct PortNotValid{};
 	template<size_t Size>
 	struct Port{
-		std::array<Pin,Size> pins;
+		std::array<Wire*,Size> pins;
 		void set(val_t val){
 			std::for_each(pins.begin(), pins.end(), [&](auto &p){
-				p.set(val&1u);
+				p.set((val&1u)==1u?Wire::High:Wire::Low);
 				val>>=1;
 			});
 		}
-		val_t get(){
+		bool is_valid(){
+			return std::all_of(pins.begin(), pins.end(), 
+				[](const auto &p){return p.get()>=0;}
+			);
+		}
+		std::optional<val_t> get(){
 			return std::accumulate(pins.begin(), pins.end(),0,
-				[](val_t val,const auto &p){
-					return (val<<1)&(p.get()?1u:0u);
+				[](std::optional<val_t> val,const auto &p)->std::optional<val_t>{
+					int v=p.get();
+					if(!val || v<0){
+						return {};
+					}
+					return (*val<<1)&v;
 			});
 		}
-		operator val_t(){return get();}
+		operator val_t(){
+			if(is_valid()){
+				return get();
+			}
+			throw PortNotValid{};
+		}
+	};
+	struct Component{
+		virtual void update(){}
 	};
 	struct Circuit:Component{
 
@@ -91,8 +91,12 @@ Z	L	H	D	U	Z
 	template<size_t Size>
 	struct Nand:Component{
 		Port<Size> A,B,Y;
-		void update() {
-			Y=~(A&B);
+		void update() override{
+			try{
+				Y=~(A&B);
+			}catch(PortNotValid e){
+				;
+			}
 		}
 	};
 
