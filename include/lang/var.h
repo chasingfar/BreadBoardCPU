0ad8cd79b8639c1dc2752b03bb8b01b9a3ef0930
chasingfar <chasingfar@gmail.com>
2022-08-08 02:40:34 +0800

    pick offset to MemVar

--- include/lang/var.h
+++ include/lang/var.h
@@ -66,7 +66,8 @@ namespace BBCPU::Lang {
 	};
 	struct MemVar:Var{
 		addr_t size;
-		explicit MemVar(addr_t size):size(size){}
+		offset_t offset;
+		explicit MemVar(addr_t size,offset_t offset):size(size),offset(offset){}
 		virtual Code load(offset_t index) const=0;
 		virtual Code save(offset_t index) const=0;
 		virtual std::shared_ptr<MemVar> shift(offset_t shift_offset,addr_t new_size) const=0;
@@ -86,8 +87,7 @@ namespace BBCPU::Lang {
 		}
 	};
 	struct LocalVar:MemVar{
-		offset_t offset;
-		explicit LocalVar(addr_t size,offset_t offset):MemVar(size),offset(offset){}
+		explicit LocalVar(addr_t size,offset_t offset):MemVar(size,offset){}
 		static auto make(addr_t size,offset_t offset){ return std::make_shared<LocalVar>(size,offset);}
 		Code load(offset_t index) const override{
 			return load_local(offset+index);
@@ -101,8 +101,7 @@ namespace BBCPU::Lang {
 	};
 	struct StaticVar:MemVar{
 		Label label;
-		offset_t offset;
-		StaticVar(addr_t size,Label label,offset_t offset):MemVar(size),label(std::move(label)),offset(offset){}
+		StaticVar(addr_t size,Label label,offset_t offset):MemVar(size,offset),label(std::move(label)){}
 		static auto make(addr_t size,const Label& label,offset_t offset){ return std::make_shared<StaticVar>(size,label,offset);}
 		Code load(offset_t index) const override{
 			return Ops::load(label,offset+index);
@@ -116,8 +115,7 @@ namespace BBCPU::Lang {
 	};
 	struct PtrVar:MemVar{
 		Code ptr;
-		offset_t offset;
-		explicit PtrVar(addr_t size,Code ptr,offset_t offset=0):MemVar(size),ptr(std::move(ptr)),offset(offset){}
+		explicit PtrVar(addr_t size,Code ptr,offset_t offset=0):MemVar(size,offset),ptr(std::move(ptr)){}
 		static auto make(addr_t size,const Code& ptr,offset_t offset=0){ return std::make_shared<PtrVar>(size,ptr,offset);}
 		Code load(offset_t index) const override{
 			return {ptr,Ops::load(offset+index)};
