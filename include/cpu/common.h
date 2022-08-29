059c9dc6a91ae5de8a1861818062d6e7a3a35325
chasingfar <chasingfar@gmail.com>
2022-08-29 14:36:07 +0800

    rewrite Logger

--- include/cpu/common.h
+++ include/cpu/common.h
@@ -8,6 +8,7 @@
 #include "../Bitwise.h"
 #include <functional>
 inline std::vector<std::string> logs{};
+inline std::string logout{};
 enum struct LogState{
 	Start,Step,Stop
 };
@@ -18,34 +19,31 @@ inline std::string log_arg(T0 v0, T... value){
 	ss<<"("<<v0<<((ss << ',' << value), ...,")");
 	return ss.str();
 }
+inline std::string log_arg(){
+	return "()";
+}
 struct Logger{
 	explicit Logger(const std::string& str){
 		logs.emplace_back(str);
 		if(state==LogState::Step){print();stop();}
 	}
 	~Logger(){
+		if(state==LogState::Step){print();stop();}
 		logs.pop_back();
 	}
 	static void print(){
 		for(const auto& log:logs){
-			std::cout<<":"<<log;
+			logout+=":"+log;
 		}
 	}
-	static void start(){state=LogState::Start;}
+	static void start(){state=LogState::Start;logout="";}
 	static void stop(){state=LogState::Stop;}
 	static void step(){state=LogState::Step;}
 };
-#if CPU_DEBUG
 #define LOG(...) Logger __{__func__+log_arg(__VA_ARGS__)};
 #define LOG_START() Logger::start();
 #define LOG_STOP() Logger::stop();
 #define LOG_STEP() Logger::step();
-#else
-#define LOG(...)
-#define LOG_START()
-#define LOG_STOP()
-#define LOG_STEP()
-#endif
 namespace BBCPU {
 	using namespace Util::Bitwise::BitField;
 	using Util::TruthTable;
